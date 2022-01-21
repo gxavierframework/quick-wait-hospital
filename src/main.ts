58c97abb10fe648b93d8c24ba35830f8788f96b7
Victor <victorafonso@frwk.com.br>
2022-01-21 10:16:28 -0300

    Implementation single spa route

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,27 @@
 import { h, createApp } from "vue";
 import singleSpaVue from "single-spa-vue";
-
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-
+import "./set-public-path";
 import "./main.css";
 
 const vueLifecycles = singleSpaVue({
   createApp,
   appOptions: {
     render() {
-      return h(App, {
+      return h(App);
+      // , {
         // single-spa props are available on the "this" object. Forward them to your component as needed.
         // https://single-spa.js.org/docs/building-applications#lifecyle-props
-        // if you uncomment these, remember to add matching prop definitions for them in your App.vue file.
-        /*
-        name: this.name,
-        mountParcel: this.mountParcel,
-        singleSpa: this.singleSpa,
-        */
-      });
+        // name: this.name,
+        // mountParcel: this.mountParcel,
+        // singleSpa: this.singleSpa,
+      // }
     },
   },
-  handleInstance(app) {
+  handleInstance: (app) => {
     app.use(router);
-    app.use(store);
   },
 });
 
